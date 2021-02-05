ee55fcde86a637afd1298375123457dad37fc825
Zentraxus <58821444+Zentraxus@users.noreply.github.com>
2021-02-05 14:01:41 +0100

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+The sum of the squares of the first ten natural numbers is,
+The square of the sum of the first ten natural numbers is,
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+.
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+
+difference = ((100*101)/2)**2 - (1/6)*(100*101*201) #sum formula from Gauß / i just typed it in a calculator
+print(difference)
