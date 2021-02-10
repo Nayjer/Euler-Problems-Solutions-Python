2f80e6b77fdf4f948e7cb879ea1f43d7d4f24497
Nayjer <58821444+Zentraxus@users.noreply.github.com>
2021-02-10 20:28:10 +0100

    Update 14.py

--- 14.py
+++ 14.py
@@ -9,10 +9,7 @@ for i in range(2, 1000000):
 		elif i % 2 != 0:
 			lenght += 1
 			i = 3*i+1
-	print(lenght)
 	lenghts.append(lenght)
-
-print(max(lenghts))
 
 index = lenghts.index(max(lenghts))
 print(index)
